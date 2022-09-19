c4c615ab711151c322d1ca76f0f42b9678699a2d
Abhijeet B Kurade <64726465+abhikurade@users.noreply.github.com>
2022-09-19 23:32:26 +0530

    Some new commits

---
+++ src/main/java/Greeshma.java
@@ -0,0 +1,14 @@
+public class Greeshma {
+    public static void main(String[] args) {
+        System.out.println(getSwapTime("001011"));
+    }
+    public static int getSwapTime(String color){
+        int n = color.length();
+        StringBuilder colors = new StringBuilder(color);
+        boolean changed = false;
+        int time = 0;
+        
+
+        return time;
+    }
+}
